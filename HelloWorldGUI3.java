57aa071ccc3dd10a23df7bdd0130443e2d7c1130
dstanchevici <distanchev@gmail.com>
2023-03-30 14:23:08 -0400

    5.14

--- HelloWorldGUI3.java
+++ HelloWorldGUI3.java
@@ -2,6 +2,7 @@
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Arc2D;
 import javax.swing.*;
 import javax.swing.event.*;
 
@@ -18,8 +19,24 @@ class DrawPanel extends JPanel {
 	g.setColor (Color.blue);
 	g.drawString ("Hello World!", 350, 200);
 
-	g.setColor (Color.black);
-	g.drawOval (70, 90, 200, 250);
+	Graphics2D g2 = (Graphics2D) g;
+	g2.setStroke(new BasicStroke(2));
+	g2.setColor (Color.black);
+	// Face
+	g2.drawOval (70, 90, 200, 250);
+	// Eyes
+	g2.drawOval (95,160, 55, 40);
+	g2.drawOval (190,160, 55, 40);
+	g2.fillOval (115, 175, 15, 15);
+	g2.fillOval (210, 175, 15, 15);
+	// Ears
+	g2.drawOval (45,195, 25, 50);
+	g2.drawOval (270,195, 25, 50);
+	// Nose
+	g2.drawOval (145,205, 50, 50);
+	// Mouth
+	g2.fill(new Arc2D.Double(100, 210, 140, 100, 200, 140,Arc2D.CHORD));
+
     }
     
 }
